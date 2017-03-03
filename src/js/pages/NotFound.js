d92d1ce0b36ef174f722c50a587d317c16a17cc8
wnamen <williamjosephnamen@gmail.com>
2017-03-02 17:16:06 -0800

    completed server error and adjusted styling of 404

--- src/js/pages/NotFound.js
+++ src/js/pages/NotFound.js
@@ -8,7 +8,7 @@ class NotFound extends React.Component {
     	<div class="ten offset-by-three white-background card flex-row-center">
 				<div class="nine columns flex-column-center">
 					<h1 class="electric-blue font-size-four small-vertical-margin"><strong class="largeText">404 Error</strong></h1>
-					<h4 class="electric-blue font-size-two medium-vertical-margin">Oh no! This page is missing</h4>
+					<h4 class="electric-blue font-size-one-and-half medium-vertical-margin">Oh no! This page is missing</h4>
 			  	<IndexLink to="/" class="active button-empty button-fill large-vertical-margin">Take me home!</IndexLink>
 				</div>
 			</div>
--- src/js/pages/ServerError.js
+++ src/js/pages/ServerError.js
@@ -4,15 +4,12 @@ import { IndexLink }          from 'react-router';
 class ServerError extends React.Component {
   render = () => {
     return (
-      <div class="ten offset-by-three white-background onboardingCard">
-        <div class="sixteen columns">
-          <div class="nine columns onbMargin text-center">
-            <img class="modalIcon" src="/src/img/500error.png" />
-            <h1 class="modalTitle electric-blue onbTitle"><strong class="largeText">Server Error</strong></h1>
-            <h1 class="modalTitle electric-blue onbTitle">We're sorry you're experiencing this, <br /> our developers
-              have been notified.</h1>
-            <IndexLink to="/" class="active">Take me home!</IndexLink>
-          </div>
+      <div class="ten offset-by-three white-background card flex-row-center">
+        <div class="nine columns flex-column-center">
+          <h1 class="electric-blue font-size-four small-vertical-margin"><strong class="largeText">Server Error</strong></h1>
+          <h4 class="electric-blue font-size-one-and-half medium-vertical-margin text-center">We're sorry you're experiencing this, our developers
+            have been notified.</h4>
+          <IndexLink to="/" class="active button-empty button-fill large-vertical-margin">Take me home!</IndexLink>
         </div>
       </div>
     );
