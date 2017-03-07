7b74145d02179ee8a15a07a50de18fa364a1a6c5
wnamen <williamjosephnamen@gmail.com>
2017-03-07 15:04:52 -0800

    added landing page navigation

--- src/js/components/landing/Header.js
+++ src/js/components/landing/Header.js
@@ -1,4 +1,5 @@
 import React, { Component }         from 'react';
+import { IndexLink }                from 'react-router';
 
 class Header extends React.Component {
   render = () => {
@@ -7,6 +8,10 @@ class Header extends React.Component {
         <section class="header-container electric-blue-light-background flex-column-center">
           <h1 class="font-size-four small-vertical-margin">React Boilerplate</h1>
           <h6 class="gray font-size-one-and-half medium-vertical-margin">Start building with React today!</h6>
+          <div class="large-margin">
+            <IndexLink class="landing-button-empty medium-margin electric-blue-border small-border" to="/signup">Sign Up</IndexLink>
+            <IndexLink class="landing-button-empty medium-margin electric-blue-border small-border" to="/login">Login</IndexLink>
+          </div>
         </section>
       </div>
     )
